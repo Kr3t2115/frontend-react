555d9577a26b242b9ab2f254695bb05c2c950748
Kr3t2115 <szymon.baranowicz@teb.edu.pl>
2023-02-15 00:00:15 +0100

    change homePage and navbar , remove react.svg

--- src/pages/homePage/homePage.tsx
+++ src/pages/homePage/homePage.tsx
@@ -2,8 +2,6 @@ import Navbar from "../../components/navbar/navbar";
 import "./homePage.css";
 
 export default function HomePage() {
-  console.log(import.meta.env.VITE_API_KEY);
-
   document.title = "Strona Główna";
 
   return (
